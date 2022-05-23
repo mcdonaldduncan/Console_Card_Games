258c7f6fbcdc84af69b8a79b7ce1265db70ffe93
mcdonaldduncan <380939@colum.edu>
2022-05-23 16:41:08 -0500

    add project

---
+++ TestProj/TestProj/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "Duncan";
+            string anotherName = "Duncan";
+
+            string thirdName = new string("Duncan");
+
+            Console.WriteLine(name == thirdName);
+
+            
+            
+        }
+    }
+}
